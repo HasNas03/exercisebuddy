c71f4ebd856294c975122704fa95cc8ab5c4e5c7
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2024-01-04 00:03:56 -0500

    started react app

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        < Navbar/>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div className="home">
+            <h2>Home Page</h2>
+        </div>
+    )
+}
+
+export default Home
