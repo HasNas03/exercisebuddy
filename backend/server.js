3823b158a680ed913de41ee9191a86f21ddadbb8
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2024-01-03 22:38:07 -0500

    done connecting to mongodb

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ require('dotenv').config() // requiring env file for env variables such as port
 // 1. first we need to require the express package
 const express = require('express')
 
+//2. require mongodb
+const mongoose = require('mongoose')
+
 const workoutRoutes = require('./routes/workouts')
 // 1. start the express app
 const app = express()
@@ -16,10 +19,18 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-// 1. listen for requests
-app.listen(process.env.PORT, () => {
-    console.log("listening on port: ", process.env.PORT) // >>> nodemon server.js
-})
+// 2. connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            // 1. listen for requests
+            app.listen(process.env.PORT, () => {
+            console.log("connected to db & listening on port: ", process.env.PORT)})// >>> nodemon server.js
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+
 
 
 
