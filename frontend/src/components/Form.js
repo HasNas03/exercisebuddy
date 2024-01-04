a6c4b21dfa43a34c6d767bf26e20698b35b191a8
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2024-01-04 02:41:22 -0500

    added css

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -42,9 +42,9 @@ const WorkoutForm = () => {
 
   return (
     <form className="create" onSubmit={handleSubmit}> 
-      <h3>Add a New Workout</h3>
+      <h3>Add New Workout</h3>
 
-      <label>Excersize Title:</label>
+      <label>Exersize Name:</label>
       <input 
         type="text" 
         onChange={(e) => setTitle(e.target.value)} 
