0aa7e3ce37e5a6b8572d1cf4cfa1a550916f5258
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2024-01-04 00:33:35 -0500

    added card for exercise

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+import WorkoutDetails from '../components/WorkoutDetails'
+
 function Home(){
+    // fetch workouts via api from backend 
+
+    const[workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok){
+                setWorkouts(json)
+            }
+        }
+        fetchWorkouts()
+    }, [])
+    
     return(
         <div className="home">
-            <h2>Home Page</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => ( // only map to list (present) if workouts exist
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+                ))}
+            </div>
         </div>
     )
 }
